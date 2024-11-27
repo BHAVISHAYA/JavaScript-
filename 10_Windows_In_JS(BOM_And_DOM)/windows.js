d584d1e51e29d4fbe38d6ad17fbed9f867d77ce0
BHAVISHAYA <bhavishayakhandelwal@gmail.com>
2024-11-27 15:18:20 +0530

    DOM

---
+++ 10_Windows_In_JS(BOM_And_DOM)/windows.js
@@ -0,0 +1,14 @@
+//? Window :- 
+//* 1. Window is the main container, or we can say the GLOBAL OBJECT and any operations related to entire browser window can be a part of window object. 
+
+//* 2. All the members like objects, methods or properties. If they are the part of window object, them we do not refer the window object. 
+
+//* 3. Window has methods, properties and object. Example :- setTimeout() or setInterval() are the methods, where as Document is the object of the Window and it also has a screen object with properties describing the physical display. 
+
+
+//? Document :- 
+//* 1. Whereas the DOM is the child of the window object. 
+
+//* 2. Where in the DOM we need to refer the doceument, if we want to use the document object, methods or properties.
+
+//* 3. Document is just the OBJECT of the GLOBAL OBJECT that is Window, which deals with the document, the HTML elements themselves. 
